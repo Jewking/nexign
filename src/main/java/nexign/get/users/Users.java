5f0baff8f3a4980d7b255791a9f21828dcf915fa
Jewking <csl1139753@gmail.com>
2020-11-14 23:40:36 +0300

    Initial commit

---
+++ src/main/java/nexign/get/users/Users.java
@@ -0,0 +1,88 @@
+package nexign.get.users;
+
+import java.time.LocalDateTime;
+
+public final class Users {
+    public static int userCounter = 0;
+    public static int cardCounter = 0;
+
+    private int id;
+    private String name;
+    private LocalDateTime dateCreated;
+
+    public Users(String name) {
+        this.id = userCounter++;
+        this.name = name;
+        this.dateCreated = LocalDateTime.now();
+    }
+
+    private class Card {
+        private int id;
+        private int money;
+        private int limit;
+        private boolean done;
+
+        public Card() {
+            this.id = cardCounter++;
+            this.money = 0;
+            this.limit = 0;
+            this.done = false;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public int getMoney() {
+            return money;
+        }
+
+        public void setMoney(int money) {
+            this.money = money;
+        }
+
+        public int getLimit() {
+            return limit;
+        }
+
+        public void setLimit(int limit) {
+            this.limit = limit;
+        }
+
+        public boolean isDone() {
+            return done;
+        }
+
+        public void setDone(boolean done) {
+            this.done = done;
+        }
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDateTime getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDateCreated(LocalDateTime dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+}
---
+++ src/main/java/nexign/get/users/rest/UsersRestController.java
@@ -0,0 +1,67 @@
+package nexign.get.users.rest;
+
+import nexign.get.users.Users;
+import nexign.get.users.exception.NotFoundException;
+import org.apache.catalina.User;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("api")
+class UsersRestController {
+    private ArrayList<Users> users = new ArrayList<Users>() {
+        {
+            add(new Users("Arslan Gareev"));
+            add(new Users("Leonid Rakitin"));
+            add(new Users("Nastya Lipuchka"));
+        }
+    };
+
+    private Users getUserID(@PathVariable int id) {
+        return users.stream()
+                .filter(user -> user.getId() == id)
+                .findFirst()
+                .orElseThrow(NotFoundException::new);
+    }
+
+    private Users getUserCardsID(@PathVariable int id) {
+        return users.stream()
+                .filter(user -> user.getId() == id)
+                .findFirst()
+                .orElseThrow(NotFoundException::new);
+    }
+    @GetMapping
+    public ArrayList<Users> list () {
+        return users;
+    }
+
+    @GetMapping("{id}")
+    public Users getId(@PathVariable int id) {
+        return getUserID(id);
+    }
+
+    @GetMapping("cards/{id}")
+    public Users getUserCards(@PathVariable int id) {
+        return getUserCardsID(id);
+    }
+
+    @PostMapping
+    public String add(@RequestBody String name ) {
+        users.add(new Users(name));
+        return name;
+    }
+
+    @PutMapping("{id}")
+    public Users update(@PathVariable int id, @RequestBody String name) {
+        Users userFound = getUserID(id);
+        userFound.setName(name);
+        return userFound;
+    }
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable int id) {
+        Users userFound = getUserID(id);
+        users.remove(userFound);
+    }
+}
